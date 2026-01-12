366d032ccc73ac4f07e6407bcf519e2cccfe35eb
Dhruvakumar <dhruvakumarvm@gmail.com>
2026-01-12 23:09:15 +0530

    Update the comments only

--- remove_dup.py
+++ remove_dup.py
@@ -11,7 +11,7 @@ class Solution(object):
             return 0
 
         k = 1
-        for i in range(1,len(nums)):  
+        for i in range(1,len(nums)):  #Start from second element
             if nums[i] != nums[i-1]:  #compare current element with previous one
                 nums[k] = nums[i]     #place unique element at index k
                 k += 1
